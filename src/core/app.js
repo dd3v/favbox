574ffdfca6b5204d7802ca6c46328a3e7d4c2fff
Dmitry Magalyas <dmitry.magalyas@gmail.com>
2023-01-15 16:33:02 +0100

    tailwind ui

--- src/core/app.js
+++ src/core/app.js
@@ -1,12 +1,5 @@
 import { createApp } from 'vue';
 import App from '@/App.vue';
-import Antd, { ConfigProvider } from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.variable.min.css';
+import '@/styles/app.css';
 
-ConfigProvider.config({
-  prefixCls: 'ant',
-  theme: {
-    primaryColor: '#000000',
-  },
-});
-createApp(App).use(Antd).mount('#app');
+createApp(App).mount('#app');
--- src/views/AppView.vue
+++ src/views/AppView.vue
@@ -1,61 +1,65 @@
+<!-- eslint-disable max-len -->
 <template>
-  <a-layout has-sider>
-    <a-layout-sider
-      :style="{
-        overflow: 'auto',
-        height: '100vh',
-        position: 'fixed',
-        left: 0,
-        top: 0,
-        bottom: 0,
-        background: '#fff',
-      }"
-    >
-      sidebar
-    </a-layout-sider>
-    <a-layout :style="{ marginLeft: '200px' }">
-      <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%', background: '#fff' }">
-        <a-row>
-          <a-col :span="8">
-            <a-input />
-          </a-col>
-        </a-row>
-      </a-layout-header>
-      <a-layout-content :style="{ margin: '70px 16px 0', overflow: 'initial' }">
-        <a-row :gutter="10">
-          <a-col :span="6" v-for="(bookmark, key) in bookmarks" :key="key">
-            <a-card size="small" hoverable bordered style="width: 100%">
-              <template #cover>
-                <img alt="example" :src="bookmark.image" />
-              </template>
-              <template #title></template>
-              <template #actions>
-                <setting-outlined key="setting" />
-                <edit-outlined key="edit" />
-                <ellipsis-outlined key="ellipsis" />
-              </template>
-              <a-card-meta :description="bookmark.description">
-                <template #avatar>
-                  <a-avatar :src="bookmark.favicon" />
-                </template>
-                <template #title>
-                  {{ bookmark.title }}
-                </template>
-              </a-card-meta>
-            </a-card>
-          </a-col>
-        </a-row>
-      </a-layout-content>
-    </a-layout>
-  </a-layout>
+  <div class="mx-auto">
+    <div class="flex flex-row">
+      <div
+        class="flex h-screen sticky top-0 w-50 px-4 bg-white border-r dark:bg-gray-900 dark:border-gray-700"
+      >
+        <div class="flex flex-col items-center justify-center h-screen vh-screen">
+          <filter-tabs v-model="tab" :items="tabs" />
+        </div>
+      </div>
+
+      <filter-list :items="folders" v-if="tab === 'categories'"/>
+      <filter-list v-if="tab === 'tags'"/>
+      <filter-list v-if="tab === 'domains'"/>
+
+      <div
+        class="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-5 py-5 bg-neutral-100"
+      >
+        <div
+          class="w-full inline-block max-w-sm overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 duration-300 hover:-translate-y-1 cursor-pointer"
+          v-for="(bookmark, key) in bookmarks"
+          :key="key"
+        >
+          <img class="object-cover object-center w-full h-56" :src="bookmark.image" alt="avatar" />
+
+          <div class="flex items-center px-6 py-3 bg-gray-900">
+            <img :src="bookmark.favicon" alt="image" width="16" />
+
+            <h1 class="mx-3 text-sm font-semibold text-white">{{ bookmark.domain }}</h1>
+          </div>
+
+          <div class="px-6 py-4">
+            <h1 class="text-md font-semibold text-gray-800 dark:text-white">
+              {{ bookmark.title }}
+            </h1>
+
+            <p class="py-2 text-gray-700 dark:text-gray-400">
+              {{ bookmark.description }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
+import FilterTabs from '@/components/FilterTabs.vue';
+import FilterList from '@/components/FilterList.vue';
 import Bookmark from '@/storage/bookmark';
 import initStorage from '@/storage/idb/idb';
 import { getBookmarkFolders } from '@/helpers/folders';
-import { toRaw, reactive, ref } from 'vue';
-import { SettingOutlined, EditOutlined, EllipsisOutlined } from '@ant-design/icons-vue';
+import {
+  toRaw, reactive, ref, watch,
+} from 'vue';
 
+const tabs = [
+  { label: 'Categories', value: 'categories', icon: '' },
+  { label: 'Tags', value: 'tags', icon: '' },
+  { label: 'Domains', value: 'domains', icon: '' },
+];
+const tab = ref();
 const bookmarks = ref([]);
 await initStorage();
 const bookmark = new Bookmark();
@@ -69,10 +73,11 @@ const conditions = reactive({ ...defaultConditions });
 console.warn(conditions);
 bookmarks.value = await bookmark.search(toRaw(conditions));
 const folders = await getBookmarkFolders();
+const tags = await bookmark.getTags();
+const domains = await bookmark.getDomains();
+console.log(tags);
+console.warn(domains);
 console.log(folders);
+watch(tab, () => console.warn(tab));
 </script>
-<style>
-.site-layout .site-layout-background {
-  background: #fff;
-}
-</style>
+<style></style>
