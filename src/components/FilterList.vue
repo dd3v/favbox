069c594ab07c3145586063f6040e80ae4ae9e90c
Dmitry Magalyas <dmitry.magalyas@gmail.com>
2023-01-17 20:01:40 +0100

    split components :zap:

--- src/components/FilterList.vue
+++ src/components/FilterList.vue
@@ -1,5 +1,54 @@
+<!-- eslint-disable max-len -->
 <template>
-  <h1>Filter list here</h1>
+  <div class="flex h-full sticky top-0">
+    <div class="min-h-screen bg-white p-3">
+      <div class="relative flex items-center w-full h-12 overflow-hidden">
+        <div class="grid place-items-center h-full w-12 text-gray-300">
+          <magnifying-glass-circle-icon class="w-6 h-6" />
+        </div>
+        <input
+          class="h-full w-full outline-none text-gray-500"
+          type="text"
+          id="search"
+          placeholder="Search something.."
+        />
+      </div>
+      <label
+        :for="`${item + key}`"
+        class="flex place-items-end cursor-pointer text-gray-700 hover:text-grey-900 hover:bg-neutral-100 rounded-md px-2 py-2 my-2"
+        v-for="(item, key) in items"
+        :key="key"
+      >
+        <span
+          class="h-2 w-2 m-1 rounded-full"
+          :class="selected.includes(item) ? 'bg-gray-500' : 'bg-gray-300'"
+        ></span>
+        <input
+          type="checkbox"
+          class="hidden"
+          name="item"
+          :id="`${item + key}`"
+          :value="item"
+          v-model="selected"
+        />
+        {{ item }}
+      </label>
+    </div>
+  </div>
 </template>
-<script setup></script>
-<style scoped></style>
+<script setup>
+import { MagnifyingGlassCircleIcon } from '@heroicons/vue/24/outline';
+import { ref } from 'vue';
+
+defineProps({
+  modelValue: {
+    type: Array,
+  },
+  items: {
+    type: Array,
+    required: true,
+  },
+});
+
+const selected = ref([]);
+</script>
--- src/views/AppView.vue
+++ src/views/AppView.vue
@@ -1,22 +1,30 @@
 <!-- eslint-disable max-len -->
 <template>
-  <filter-tabs/>
+  <div class="flex">
+    <nav-sidebar :items="tabs" />
+    <filter-list :items="domains" />
+    <div class="flex px-4 py-4">
+      <bookmark-card v-for="(bookmark, key) in bookmarks" :key="key"/>
+    </div>
+  </div>
 </template>
 <script setup>
-import FilterTabs from '@/components/FilterTabs.vue';
-// import FilterList from '@/components/FilterList.vue';
-import Bookmark from '@/storage/bookmark';
-import initStorage from '@/storage/idb/idb';
-import { getBookmarkFolders } from '@/helpers/folders';
 import {
   toRaw, reactive, ref, watch,
 } from 'vue';
+import NavSidebar from '@/components/NavSidebar.vue';
+import FilterList from '@/components/FilterList.vue';
+import BookmarkCard from '@/components/BookmarkCard.vue';
+import { FolderOpenIcon, HashtagIcon, GlobeAltIcon } from '@heroicons/vue/24/outline';
+import Bookmark from '@/storage/bookmark';
+import initStorage from '@/storage/idb/idb';
+import { getBookmarkFolders } from '@/helpers/folders';
 
-// const tabs = [
-//   { label: 'Categories', value: 'categories', icon: '' },
-//   { label: 'Tags', value: 'tags', icon: '' },
-//   { label: 'Domains', value: 'domains', icon: '' },
-// ];
+const tabs = [
+  { label: 'Folder', value: 'folder', icon: FolderOpenIcon },
+  { label: 'Tag', value: 'tag', icon: HashtagIcon },
+  { label: 'Domain', value: 'domain', icon: GlobeAltIcon },
+];
 const tab = ref();
 const bookmarks = ref([]);
 await initStorage();
