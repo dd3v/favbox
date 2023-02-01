851269d67c0d6931dbd93d6247acd36acec4bfdb
Dmitry Magalyas <dmitry.magalyas@gmail.com>
2023-02-01 12:22:14 +0100

    display types and layout

--- src/components/bookmark/BookmarkLayout.vue
+++ src/components/bookmark/BookmarkLayout.vue
@@ -1,11 +1,31 @@
 <template>
-    <h1>Layout</h1>
+  <div :class="layout">
+    <slot />
+  </div>
 </template>
 <script setup>
-defineProps({
+import { computed } from 'vue';
+
+const props = defineProps({
   displayType: {
     type: String,
     required: true,
+    default: 'masonry',
+  },
+});
+
+const layout = computed({
+  get: () => {
+    switch (props.displayType) {
+      case 'masonry':
+        return 'columns-2 md:columns-3 lg:columns-4';
+      case 'card':
+        return 'grid gap-10 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4';
+      case 'list':
+        return 'grid gap-5 grid-cols-1';
+      default:
+        return 'columns-2 md:columns-3 lg:columns-4';
+    }
   },
 });
 </script>
--- src/components/bookmark/BookmarkMasonry.vue
+++ src/components/bookmark/BookmarkMasonry.vue
@@ -1,16 +1,15 @@
 <!-- eslint-disable max-len -->
 <template>
-  <div
-    class="relative mb-6 w-full border border-solid max-w-sm overflow-hidden  bg-white shadow-sm"
-  >
+  <div class="relative mb-6 w-full max-w-sm overflow-hidden border border-solid bg-white shadow-sm">
     <a :href="bookmark.url" target="_blank">
       <v-lazy-image
         class=""
-        :src="bookmark.image ?? ''"
+        :src="bookmark.image ?? placeholder"
         :alt="bookmark.title"
+        @error="errorHandler"
       />
       <div class="flex items-center bg-gray-600 p-1">
-        <bookmark-favicon :favicon="bookmark.favicon" class="h-4 w-4"/>
+        <bookmark-favicon :favicon="bookmark.favicon" class="h-4 w-4" />
         <span class="mx-3 text-sm font-semibold text-white">{{ bookmark.domain }}</span>
       </div>
       <div class="px-6 py-4">
@@ -28,14 +27,26 @@
 <script setup>
 import VLazyImage from 'v-lazy-image';
 import BookmarkFavicon from '@/components/bookmark/BookmarkFavicon.vue';
+import { computed } from 'vue';
+import empty from '@/assets/empty.svg';
+import notfound from '@/assets/notfound.svg';
+import placeholder from '@/assets/placeholder.svg';
 
-defineProps({
+const props = defineProps({
   bookmark: {
     type: Object,
     required: true,
   },
 });
 
+const bookmark = computed({
+  get: () => props.bookmark,
+});
+
+const errorHandler = (e) => {
+  console.warn(e);
+  bookmark.value.image = notfound;
+};
 </script>
 <style scoped>
 .v-lazy-image {
