26dfcda31a94acb3b678f1e24b4f19b825caa3f5
Dmitry Magalyas <dmitry.magalyas@gmail.com>
2023-02-01 19:47:20 +0100

    preview improvements

--- src/components/BookmarkTools.vue
+++ src/components/BookmarkTools.vue
@@ -7,8 +7,8 @@
     }"
   >
     <TabGroup :selectedIndex="selectedTab" @change="changeTab">
-      <div class="flex w-full p-2">
-        <div class="flex w-full justify-center">
+      <div class="flex w-full">
+        <div class="flex w-full justify-center py-1">
           <TabList class="flex w-72 space-x-1 rounded-xl bg-gray-300/20 p-1">
             <Tab v-slot="{ selected }" as="template">
               <button
@@ -38,40 +38,47 @@
             </Tab>
           </TabList>
         </div>
-        <button @click="close">
+        <button @click="close" class="mr-2">
           <x-circle-icon class="h-5 w-5" />
         </button>
       </div>
-
-      <TabPanels class="w-full">
-        <TabPanel class="flex h-screen w-full overflow-y-auto p-4">
-          <app-spinner v-if="loading" class="flex w-full items-center justify-center" />
-          <article class="prose prose-slate">
-            <div v-html="content"></div>
+      <TabPanels class="flex h-screen w-full p-4">
+        <TabPanel class="flex h-screen w-full overflow-y-auto">
+          <article class="prose prose-slate w-full max-w-none">
+            <app-spinner
+              v-if="loading"
+              class="flex h-screen w-full flex-col items-center justify-center"
+            />
+            <div
+              class="flex h-screen w-full flex-col items-center justify-center"
+              v-if="emptyState"
+            >
+              <img class="w-32" :src="empty" alt="error" />
+              <h4>Nothing to show</h4>
+            </div>
+            <div v-else v-html="content"></div>
           </article>
         </TabPanel>
-        <TabPanel class="flex h-screen w-full justify-center p-4">
+        <TabPanel class="flex h-screen w-full justify-center">
           <bookmark-form v-model="bookmark" :folders="folders" @save="handleSave" class="w-4/6" />
         </TabPanel>
       </TabPanels>
     </TabGroup>
-    <!-- We will make the mask fill the screen while the menu is visible. Because its z-index is 1 less than that of the menu, the menu will still be displayed on top of it -->
-
   </div>
-      <div
-      class="absolute left-0 top-0 z-10 h-screen w-0 bg-black/30 transition-opacity"
-      :style="{
-        width: drawerVisible ? '100vw' : '0',
-        opacity: drawerVisible ? '0.6' : '0',
-      }"
-      @keydown="close"
-      @click="close"
-    ></div>
+  <div
+    class="absolute left-0 top-0 z-10 h-screen w-0 bg-black/30 transition-opacity"
+    :style="{
+      width: drawerVisible ? '100vw' : '0',
+      opacity: drawerVisible ? '0.6' : '0',
+    }"
+    @keydown="close"
+    @click="close"
+  ></div>
 </template>
 
 <script setup>
 import Parser from '@postlight/parser';
-import { onMounted, ref, watch } from 'vue';
+import { ref, watch } from 'vue';
 import { XCircleIcon } from '@heroicons/vue/24/outline';
 import {
   TabGroup, TabList, Tab, TabPanels, TabPanel,
@@ -80,6 +87,7 @@ import BookmarkForm from '@/components/bookmark/BookmarkForm.vue';
 import { getBookmarkFolders } from '@/helpers/folders';
 import tagHelper from '@/helpers/tags';
 import AppSpinner from '@/components/AppSpinner.vue';
+import empty from '@/assets/empty.svg';
 
 const bookmark = ref({});
 const selectedTab = ref(1);
@@ -87,6 +95,8 @@ const folders = ref([]);
 const content = ref('');
 const drawerVisible = ref(false);
 const loading = ref(true);
+const emptyState = ref(false);
+folders.value = await getBookmarkFolders();
 
 function changeTab(index) {
   selectedTab.value = index;
@@ -114,21 +124,26 @@ const open = (tabIndex, data) => {
 watch(
   bookmark,
   async () => {
-    content.value = '';
-    loading.value = true;
-    const response = await Parser.parse(bookmark.value.url);
-    content.value = response?.content;
-    loading.value = false;
+    try {
+      emptyState.value = false;
+      content.value = '';
+      loading.value = true;
+      const response = await Parser.parse(bookmark.value.url);
+      content.value = response?.content ?? '';
+      if (content.value.length <= 100) {
+        throw new Error('Nothing to show');
+      }
+    } catch (e) {
+      emptyState.value = true;
+      content.value = '';
+    } finally {
+      loading.value = false;
+    }
   },
   {
     immediate: true,
     deep: true,
   },
 );
-
-// FIXME
-onMounted(async () => {
-  folders.value = await getBookmarkFolders();
-});
 defineExpose({ open });
 </script>
