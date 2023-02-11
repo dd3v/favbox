631f63e78bc8e99d5e966793d24bc429a1afb2a0
Dmitry Magalyas <dmitry.magalyas@gmail.com>
2023-02-11 20:11:47 +0100

    error handler

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@
   <app-notifications />
 </template>
 <script setup>
+import { onErrorCaptured } from 'vue';
 import AppView from '@/views/AppView.vue';
 import AppNotifications from '@/components/AppNotifications.vue';
+import { notify } from 'notiwind';
+
+onErrorCaptured((e) => {
+  notify({ group: 'error', title: 'Error', text: e.message }, 8500);
+});
 </script>
