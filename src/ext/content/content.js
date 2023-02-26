758dc07b113fbba59bf67e5bd37e95bac500da01
Dmitry Magalyas <dmitry.magalyas@gmail.com>
2023-02-26 23:36:14 +0100

    DOMPurify

--- src/ext/content/content.js
+++ src/ext/content/content.js
@@ -1,1 +1,19 @@
-console.warn('SAVED..');
+import Parser from '@/libs/parser';
+
+console.warn('Content script..');
+let port;
+function connect() {
+  port = chrome.runtime.connect({ name: 'favbox' });
+  port.onDisconnect.addListener(connect);
+  port.onMessage.addListener((msg) => {
+    console.log('received', msg, 'from bg');
+  });
+}
+try {
+  connect();
+  const parser = new Parser(document.location.href, document);
+  const pageInfo = parser.getFullPageInfo();
+  port.postMessage({ action: 'cache', data: pageInfo });
+} catch (e) {
+  console.error('Content script error', e);
+}
