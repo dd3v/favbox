b52d0611fbd18dcdbeb6611eab248451c608aac7
Dmitry Magalyas <dmitry.magalyas@gmail.com>
2023-03-05 17:01:58 +0100

    v2 (wip)

--- src/ext/content/content.js
+++ src/ext/content/content.js
@@ -1,19 +1,29 @@
 import Parser from '@/libs/parser';
+import { createApp } from 'vue';
+import AddBookmark from './AddBookmark.vue';
 
-console.warn('Content script..');
-let port;
-function connect() {
-  port = chrome.runtime.connect({ name: 'favbox' });
-  port.onDisconnect.addListener(connect);
-  port.onMessage.addListener((msg) => {
-    console.log('received', msg, 'from bg');
-  });
-}
-try {
-  connect();
-  const parser = new Parser(document.location.href, document);
-  const pageInfo = parser.getFullPageInfo();
-  port.postMessage({ action: 'cache', data: pageInfo });
-} catch (e) {
-  console.error('Content script error', e);
-}
+const root = document.createElement('div');
+
+createApp(AddBookmark).mount(root);
+
+// Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
+(() => {
+  console.warn('Content script..');
+  let port;
+  function connect() {
+    port = chrome.runtime.connect({ name: 'favbox' });
+    port.onDisconnect.addListener(connect);
+    port.onMessage.addListener((msg) => {
+      console.log('received', msg, 'from bg');
+    });
+  }
+
+  try {
+    connect();
+    const parser = new Parser(document.location.href, document);
+    const pageInfo = parser.getFullPageInfo();
+    port.postMessage({ action: 'cache', data: pageInfo });
+  } catch (e) {
+    console.error('Content script error', e);
+  }
+})();
