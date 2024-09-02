1fddb947b379e8db8dfdc86b257150a07eb9033f
Dmitry Magalyas <dmitry.magalyas@gmail.com>
2024-09-02 17:18:36 +0200

    updated app icons

---
+++ src/components/icons/TagIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 256 256"
+  ><path
+    fill="currentColor"
+    d="M216 152h-48v-48h48a8 8 0 0 0 0-16h-48V40a8 8 0 0 0-16 0v48h-48V40a8 8 0 0 0-16 0v48H40a8 8 0 0 0 0 16h48v48H40a8 8 0 0 0 0 16h48v48a8 8 0 0 0 16 0v-48h48v48a8 8 0 0 0 16 0v-48h48a8 8 0 0 0 0-16m-112 0v-48h48v48Z"
+  /></svg>
+</template>
