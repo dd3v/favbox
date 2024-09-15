c4e06a7f23fa671a73463bf9363712516c9150aa
Dmitry Magalyas <dmitry.magalyas@gmail.com>
2024-09-15 13:26:11 +0200

    updated ui components

--- src/components/app/AppCheckbox.vue
+++ src/components/app/AppCheckbox.vue
@@ -1,25 +1,20 @@
 <template>
-  <label class="group flex cursor-pointer items-center space-x-2">
+  <label
+    :for="'checkbox-' + value"
+    class="flex cursor-pointer items-center space-x-2"
+  >
     <input
+      :id="'checkbox-' + value"
       :value="value"
+      :name="name"
       type="checkbox"
-      class="peer hidden"
+      class="size-4 rounded border-gray-300 text-black focus:ring-0 dark:border-neutral-700 dark:bg-neutral-800 dark:checked:bg-black dark:focus:ring-offset-gray-800"
       :checked="modelValue.includes(value)"
-      @input="update"
+      @change="update"
     >
-    <span
-      class="relative flex h-4 w-4 items-center justify-center rounded-sm border-2 border-gray-300 bg-white shadow-sm transition-transform duration-200 ease-in-out group-hover:border-neutral-400 peer-checked:border-neutral-400 peer-checked:bg-neutral-400 dark:border-neutral-600 dark:bg-neutral-800 dark:group-hover:border-neutral-500 dark:peer-checked:border-neutral-500 dark:peer-checked:bg-neutral-500"
-    >
-      <span
-        class="absolute inset-0 flex items-center justify-center text-white opacity-0 transition-opacity duration-200 ease-in-out peer-checked:opacity-100"
-      >
-        <span class="h-2.5 w-2.5 rounded-sm bg-current" />
-      </span>
-    </span>
-    <span class="text-gray-700 dark:text-neutral-200">{{ label }}</span>
+    <span class="text-gray-900 dark:text-gray-100">{{ label }}</span>
   </label>
 </template>
-
 <script setup>
 const emit = defineEmits(['update:modelValue']);
 
@@ -33,6 +28,10 @@ const props = defineProps({
     required: true,
   },
   value: {
+    type: String,
+    required: true,
+  },
+  name: {
     type: String,
     required: true,
   },
--- src/components/app/AppRadio.vue
+++ src/components/app/AppRadio.vue
@@ -3,30 +3,25 @@
     <input
       v-model="model"
       type="radio"
-      name="sort"
-      :value="props.value"
-      class="peer hidden"
+      :name="name"
+      :value="value"
+      class="size-4 rounded-full border-gray-300 text-black focus:ring-0 dark:border-neutral-700 dark:bg-neutral-800 dark:checked:bg-black dark:focus:ring-offset-gray-800"
     >
-    <span
-      class="relative flex size-4 items-center justify-center rounded-full border-2 border-gray-300 bg-white shadow-sm transition-transform duration-200 ease-in-out group-hover:border-neutral-400 peer-checked:border-neutral-400 peer-checked:bg-neutral-200 dark:border-neutral-600 dark:bg-neutral-800 dark:group-hover:border-neutral-500 dark:peer-checked:border-neutral-500 dark:peer-checked:bg-neutral-600"
-    >
-      <span
-        class="absolute size-2.5 rounded-full bg-neutral-600 opacity-0 transition-opacity duration-200 ease-in-out peer-checked:opacity-100 dark:bg-neutral-400"
-      />
-    </span>
-    <span class="text-gray-700 dark:text-neutral-200">{{ label }}</span>
+    <span class="text-gray-800 dark:text-neutral-100">{{ label }}</span>
   </label>
 </template>
 <script setup>
-
 const model = defineModel({ type: String, required: true });
-
-const props = defineProps({
+defineProps({
   value: {
     type: String,
     required: true,
   },
   label: {
+    type: String,
+    required: true,
+  },
+  name: {
     type: String,
     required: true,
   },
