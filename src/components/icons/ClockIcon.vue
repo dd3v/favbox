861d0205508f7f6f88eb3e223a353db806ab457d
Dmitry Magalyas <dmitry.magalyas@gmail.com>
2024-10-28 23:18:48 +0100

    new icons

---
+++ src/components/icons/ClockIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 256 256"
+  ><path
+    fill="currentColor"
+    d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24m0 192a88 88 0 1 1 88-88a88.1 88.1 0 0 1-88 88m64-88a8 8 0 0 1-8 8h-56a8 8 0 0 1-8-8V72a8 8 0 0 1 16 0v48h48a8 8 0 0 1 8 8"
+  /></svg>
+</template>
---
+++ src/components/icons/SyncErrorIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 14 14"
+  ><g
+    fill="none"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ><path d="m11 9l2-.5l.5 2" /><path d="M13 8.5A6.6 6.6 0 0 1 7 13h0a6 6 0 0 1-5.64-3.95M3 5l-2 .5l-.5-2" /><path d="M1 5.5A6.79 6.79 0 0 1 7 1h0a6 6 0 0 1 5.64 4M7 3.5v4" /><circle
+    cx="7"
+    cy="10"
+    r=".5"
+  /></g></svg>
+</template>
